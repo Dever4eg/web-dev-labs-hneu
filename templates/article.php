265060793569731df8d0cc606b1fa0d6898c03a9
Bogdan Onischenko <bogdan.onischenko@nixsolutions.com>
2020-11-28 20:04:07 +0200

    Add template for single article

--- templates/article.php
+++ templates/article.php
@@ -1,1 +1,25 @@
-<p>article</p>
+<?php
+use src\Entities\Article;
+
+/**
+ * @var Article $article;
+ */
+?>
+
+<div class="card mt-4">
+  <div class="card-header">
+    <?php echo $article->getTitle() ?>
+  </div>
+  <div class="card-body">
+    <div class="row">
+      <div class="col-md-4">
+        <img class="figure-img img-fluid rounded" src="<?php echo $article->getImage() ?>">
+      </div>
+      <div class="col-md-8">
+        <p class="card-text">
+            <?php echo $article->getText() ?>
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
